0cd180abf31125fe777b260cc0d4f650292ab8a7
Govind Sahai <gsiitbhu@gmail.com>
2015-05-03 21:04:10 +0530

    Windows here

--- Credit_JSON/CCreditLine/Output.cs
+++ Credit_JSON/CCreditLine/Output.cs
@@ -26,28 +26,29 @@ namespace CCreditLine
 
         public static void about()
         {
-            Console.Write(" - - Credit_linux - - \nVersion : 3.0.2.1\nMade By : Govind Sahai\nContact : gsiitbhu_at_gmail.com\n");
+            Console.Write(" - - Credit_linux - - \nVersion : 3.1.6.9\nMade By : Govind Sahai\nContact : gsiitbhu_at_gmail.com\n");
         }
 
         public static void help()
         {
             about();
             Console.WriteLine("\nTo use this software following commands\nwith proper syntax must be used : ");
-            
             Console.Write("\n about \t-\t-\t-\t-\t-\t: To get the info about developer");
-            Console.Write("\n add* [name] [initial Amount] [Info of Money]\t: To add a Record with [name] and [intial Amount]");
+            Console.Write("\n add [name] [initial Amount] [Info of Money]\t: To add a Record with [name] and [intial Amount]");
+            Console.Write("\n branch [branch Name]\t-\t-\t-\t: Switch branch");
             Console.Write("\n clear \t-\t-\t-\t-\t-\t: To clear all the data");
             Console.Write("\n cls \t-\t-\t-\t-\t-\t: Clear Screen");
-            Console.Write("\n delete* [name]\t-\t-\t-\t-\t: To delete a Record with [name]");
+            Console.Write("\n delete [name]\t-\t-\t-\t-\t: To delete a Record with [name]");
             Console.Write("\n exit** \t-\t-\t-\t-\t: Exit");
             Console.Write("\n help \t-\t-\t-\t-\t-\t: To get Help");
-            Console.Write("\n show* [name]\t-\t-\t-\t-\t: Show all the data associated with [name]");
+            Console.Write("\n show [name]\t-\t-\t-\t-\t: Show all the data associated with [name]");
+            Console.Write("\n showafterdate [date]\t-\t-\t-\t: show all transactions after [date]");
             Console.Write("\n showall \t-\t-\t-\t-\t: Show all Records with Total Balance");
-            Console.Write("\n showdate [date]\t-\t-\t-\t: Show all Records on current Date");
+            Console.Write("\n showdate [date]\t-\t-\t-\t: Show all Records on [date]");
             Console.Write("\n total \t-\t-\t-\t-\t-\t: Total Balance with ALL");
-            Console.Write("\n update* [name] [amount] [Info of Money]-\t: Add [amount] to Record [name]\n");
+            Console.Write("\n update [name] [amount] [Info of Money]-\t: Add [amount] to Record [name]\n");
 
-            Console.WriteLine("\n*Any input extra to the command format will be neglected!");
+            Console.WriteLine("\n*Any input extra to any command format will be neglected!");
             Console.WriteLine("**Double Enter at main Line To Exit");
         }
     }
